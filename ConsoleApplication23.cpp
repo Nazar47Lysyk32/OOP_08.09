6c56a94715f81bb7951e3bd897d67b0297ef551a
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2023-09-24 17:38:56 +0300

    Task_1

---
+++ ConsoleApplication23.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+using namespace std;
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class BinarySearchTree {
+public:
+    BinarySearchTree() : root(nullptr) {}
+
+    // Додавання ноди до дерева
+    void insert(int value) {
+        if (root == nullptr) {
+            root = new TreeNode(value);
+        }
+        else {
+            insertRecursive(root, value);
+        }
+    }
+
+    // Вивід всіх нод
+    void display() {
+        displayRecursive(root);
+        cout << endl;
+    }
+
+private:
+    TreeNode* root;
+
+    // Рекурсивна функція для додавання ноди
+    void insertRecursive(TreeNode* node, int value) {
+        if (value < node->data) {
+            if (node->left == nullptr) {
+                node->left = new TreeNode(value);
+            }
+            else {
+                insertRecursive(node->left, value);
+            }
+        }
+        else {
+            if (node->right == nullptr) {
+                node->right = new TreeNode(value);
+            }
+            else {
+                insertRecursive(node->right, value);
+            }
+        }
+    }
+
+    // Рекурсивна функція для виводу всіх нод
+    void displayRecursive(TreeNode* node) {
+        if (node != nullptr) {
+            displayRecursive(node->left);
+            cout << node->data << " ";
+            displayRecursive(node->right);
+        }
+    }
+};
+
+int main() {
+    BinarySearchTree bst;
+
+    // Додавання елементів до дерева
+    bst.insert(50);
+    bst.insert(30);
+    bst.insert(70);
+    bst.insert(20);
+    bst.insert(40);
+    bst.insert(60);
+    bst.insert(80);
+
+    
+    cout << "All nodes in the binary search tree:" << endl;
+    bst.display();
+
+    return 0;
+}
